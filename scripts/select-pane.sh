0f1e8ee3bb9b4f9a71e261bc26043d3868c4bdb4
Florian Walter <nairolf.retlaw@gmail.com>
2023-05-22 08:44:42 +0200

    add fuzzy pane selection script

---
+++ scripts/select-pane.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ -z "$TMUX" ]; then
+  echo "This script must be run from within tmux."
+  exit 1
+fi
+
+FORMAT='#{session_id}:#{window_id}:#{pane_id} #{session_name} #{pane_current_path} #{pane_current_command}'
+PANES=$(tmux list-panes -a -F "$FORMAT")
+
+SELECTION=$(echo "${PANES}" | fzf-tmux -p | awk '{print $1}')
+if [ -z "$SELECTION" ]; then
+  exit 0
+fi
+
+ARGS=(${SELECTION//:/ })
+tmux select-pane -t "${ARGS[2]}"
+tmux select-window -t "${ARGS[1]}" 
+tmux switch-client -t "${ARGS[0]}"
